3374b4dc49d0ab1aecf8338e0387f91d10009e91
Harrison Ratcliffe <harrison.ratcliffe@me.com>
2022-10-26 14:04:04 +0100

    Update AuthenticationException.php

--- src/Exceptions/AuthenticationException.php
+++ src/Exceptions/AuthenticationException.php
@@ -14,7 +14,7 @@ class AuthenticationException extends ExceptionAbstract
 
     public function handleMessage():void
     {
-        $this->message = "You are not authenticated to perform this action.";
+        $this->message = "You are not authorized to perform this action.";
     }
 
     public function handleErrors():void
