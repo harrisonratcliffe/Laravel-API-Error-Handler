5a7bdbc24e76b5a9fb1f6de38db9da6d1e08ef2a
Harrison Ratcliffe <harrison.ratcliffe@me.com>
2023-02-15 14:15:32 +0000

    Update api-error-handler.php

--- src/Config/api-error-handler.php
+++ src/Config/api-error-handler.php
@@ -1,11 +1,31 @@
 <?php
 
 return [
+    /**
+     * NotFoundException
+     */
     "Symfony\Component\HttpKernel\Exception\NotFoundHttpException" => "\harrisonratcliffe\LaravelApiErrorHandler\Exceptions\NotFoundException",
+    "Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException" => "\harrisonratcliffe\LaravelApiErrorHandler\Exceptions\NotFoundException",
+
+    /**
+     * ServerInternalException
+     */
     "ErrorException" => "\harrisonratcliffe\LaravelApiErrorHandler\Exceptions\ServerInternalException",
     "Illuminate\Database\QueryException" => "\harrisonratcliffe\LaravelApiErrorHandler\Exceptions\ServerInternalException",
+
+    /**
+     * AuthenticationException
+     */
     "Illuminate\Auth\AuthenticationException" => "\harrisonratcliffe\LaravelApiErrorHandler\Exceptions\AuthenticationException",
+
+    /**
+     * AccessDeniedException
+     */
     "Symfony\Component\HttpKernel\Exception\HttpException" => "\harrisonratcliffe\LaravelApiErrorHandler\Exceptions\AccessDeniedException",
+    "Illuminate\Auth\Access\AuthorizationException" => "\harrisonratcliffe\LaravelApiErrorHandler\Exceptions\AccessDeniedException",
+
+    /**
+     * ValidationException
+     */
     "Illuminate\Validation\ValidationException" => "\harrisonratcliffe\LaravelApiErrorHandler\Exceptions\ValidationException",
-    "Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException"=>"\harrisonratcliffe\LaravelApiErrorHandler\Exceptions\NotFoundException",
 ];
